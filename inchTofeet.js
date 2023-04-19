c5c31442a8f428d41fbd883ab120aebda3a40938
showkhin1997 <showkhin15@gmail.com>
2023-04-19 18:35:22 +0600

    first

--- inchTofeet.js
+++ inchTofeet.js
@@ -1,3 +1,4 @@
+// inch to feet
 function inchToFeet(inches) {
     let feet = inches / 12;
     return feet;
@@ -5,3 +6,13 @@ function inchToFeet(inches) {
 
 var myInces = inchToFeet(145);
 console.log(myInces.toFixed(4));
+
+// miles to kilometer
+function milesToKiloMeter(miles) {
+    let kiloMeter = miles * 1.60934;
+    return kiloMeter;
+}
+
+var givenMiles = 120;
+var getKiloMeter = milesToKiloMeter(givenMiles);
+console.log('Kilometer: ', getKiloMeter.toFixed(2));
