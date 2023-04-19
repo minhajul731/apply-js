d6ddbe5b49391c11b47e5573f19615ec44e2d9e0
showkhin1997 <showkhin15@gmail.com>
2023-04-19 20:40:10 +0600

    factorial

--- factorial.js
+++ factorial.js
@@ -1,3 +1,5 @@
+// factorial using for loop
+
 function getFactorial(number) {
     let factorial = 1;
     for (let i = 1; i <= number; i++) {
@@ -10,4 +12,58 @@ function getFactorial(number) {
 let giveFactorialNumber = 3;
 let factorialResult = getFactorial(giveFactorialNumber);
 console.log(giveFactorialNumber + '! factorial is = ', factorialResult);
+
+
+// factorial using while loop
+/*
+function getFactorial(number) {
+    let factorial = 1;
+    let i = 1;
+    while (i <= number) {
+        // console.log(i);
+        factorial = factorial * i;
+        i++;
+    }
+    return factorial;
+}
+
+let giveFactorialNumber = 3;
+let factorialResult = getFactorial(giveFactorialNumber);
+console.log(giveFactorialNumber + '! factorial is = ', factorialResult);
+*/
+
+// factorial using for loop decrement
+/*
+function getFactorial(number) {
+    let factorial = 1;
+    for (let i = number; i >= 1; i--) {
+        console.log(i);
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+let giveFactorialNumber = 3;
+let factorialResult = getFactorial(giveFactorialNumber);
+console.log(giveFactorialNumber + '! factorial is = ', factorialResult);
+*/
+
+// factorial using while loop decrement
+/* 
+function getFactorial(number) {
+    let factorial = 1;
+    let i = number;
+    while (i >= 1) {
+        console.log(i);
+        factorial = factorial * i;
+        i--;
+    }
+    return factorial;
+}
+
+let giveFactorialNumber = 3;
+let factorialResult = getFactorial(giveFactorialNumber);
+console.log(giveFactorialNumber + '! factorial is = ', factorialResult);
+*/
+
 
