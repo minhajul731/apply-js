a57f5bc64b8109d5818f38198a156eac11461326
showkhin1997 <showkhin15@gmail.com>
2023-04-20 12:53:03 +0600

    celciasTofarenheit

--- celciasTofarenheit.js
+++ celciasTofarenheit.js
@@ -1,8 +1,19 @@
+// celciousToFarenheit
 function celciousToFarenheit(celcious) {
     let farenheit = (celcious * (9 / 5)) + 32;
     return farenheit;
 }
 
 let myCelsious = 10;
-const myFarenheit = celciousToFarenheit(myCelsious);
-console.log('Todays Tempurature is ', myFarenheit, 'Farenheit');
+const farenheit = celciousToFarenheit(myCelsious);
+console.log('Todays Tempurature is ', farenheit, 'Farenheit');
+
+// FarenheitToCelcious
+function farenheitToCelcious(farenheit) {
+    let celcious = ((farenheit - 32) * 5) / 9;
+    return celcious;
+}
+
+let myFarenheit = 50;
+const myCelcious = farenheitToCelcious(myFarenheit);
+console.log('Todays Tempurature is ', myCelcious, 'celcious');
